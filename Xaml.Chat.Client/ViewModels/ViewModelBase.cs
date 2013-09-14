47fe01fe304592dc3069700eb581c03a2c0fc5cf
P-Beshkov <pavel.beshkov@gmail.com>
2013-09-14 10:55:05 +0300

    ViewModelBase.cs added

---
+++ Xaml.Chat.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Xaml.Chat.Client.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
